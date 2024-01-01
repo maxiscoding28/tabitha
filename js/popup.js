aba3cee0519d9ef285c45d8bd1c4a4328e33502a
max <max@maxs-MacBook-Air.local>
2024-01-01 15:50:18 -0700

    x

--- js/popup.js
+++ js/popup.js
@@ -13,8 +13,7 @@ document.getElementById("cmd-input").focus();
 document.addEventListener('DOMContentLoaded', function () {
   var addTabGroupButton = grabElement('create-group');
   var editExistingGroupButton = grabElement('edit-existing-group');
-  var tabGroups = grabElement("tab-groups");
-  var tabs = grabElement("tabs");
+  var tabGroupsList = grabElement("tab-groups-list");
   
   addTabGroupButton.addEventListener('click', function () {
     sendMessage("createNewGroup");
@@ -23,9 +22,8 @@ document.addEventListener('DOMContentLoaded', function () {
     sendMessage("editExistingGroup");
   });
 
-  if (tabGroups.children.length <= 0) {
-    tabGroups.innerHTML+="<div class=\"flex-center auto-margin\">Add some groups!</div>"
-    tabs.innerHTML="<div class=\"flex-center auto-margin rotate flex-column\"><img src=\"../icons/tabitha48.png\" /><div>Nothing here...</div></div>"
+  if (tabGroupsList.children.length <= 0) {
+    tabGroupsList.innerHTML="<div class=\"flex-center auto-margin rotate flex-column\"><img src=\"../icons/tabitha48.png\" /><div>Nothing here...</div></div>"
   }
 });
 
