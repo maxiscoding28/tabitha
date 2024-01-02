192fb075386df6e3d180d9046876a706a83fe076
max <maxtwinslow1@gmail.com>
2024-01-02 12:20:24 -0700

    x

--- js/createGroup.js
+++ js/createGroup.js
@@ -1,19 +1,30 @@
 function getCreateGroupButton() {
     return document.getElementById('create-group-button')
 }
-function createGroupInChrome() {
-    chrome.tabs.query({}, function(tabs){
-        let tabIds = [];
-        tabs.forEach(function(tab) {
-            if (! isTabithaTab(tab.url)) {
-                tabIds.push(tab.id)
-            } 
-        });
-        
-        chrome.tabs.group({tabIds}, function(groupInfo) {
-        })
-    });
+function formIsValidated() {
+    // Check if Color Is Selected, If Not, assign random color
 
+    // Check if Name is added, meets critera maximum 20 characters
+
+    // Check if Alias is added, maximum of 5 characters, no spaces all plaintext (no emojis or special characters)
+
+    // Check if Box is checked or not
+}
+
+function createGroupInChrome() {
+    if (formIsValidated()) {
+        chrome.tabs.query({}, function(tabs){
+            let tabIds = [];
+            tabs.forEach(function(tab) {
+                if (! isTabithaTab(tab.url)) {
+                    tabIds.push(tab.id)
+                } 
+            });
+            
+            chrome.tabs.group({tabIds}, function(groupInfo) {
+            })
+        });
+    }
 }
 
 function createGroup() {
